2ca982b12a886a88a05bc749b8936bff5f8c4ac8
Joey Reback <114630000+rebackjoseph28@users.noreply.github.com>
2024-04-11 19:34:40 -0600

    gpio-testing

--- test/audio_test.py
+++ test/audio_test.py
@@ -2,18 +2,17 @@ import pygame
 
 sound_enable = True
 
-def sound_setup():
-    pygame.mixer.init()
-    dir = "/sounds/"
+pygame.mixer.init()
+dir = "/sounds/"
 
-    sfx1 = pygame.mixer.Sound(dir + "sfx1")
-    sfx2 = pygame.mixer.Sound(dir + "sfx2")
+sfx1 = pygame.mixer.Sound(dir + "sfx1")
+sfx2 = pygame.mixer.Sound(dir + "sfx2")
 
-    sound_dict = {
-        "sfx1": sfx1,
-        "sfx2": sfx2
-    }
-
+sound_dict = {
+    "sfx1": sfx1,
+    "sfx2": sfx2
+}
+#bee
 def play_sound(filename):
     if(sound_enable):
         pygame.mixer.Sound.play(sound_dict[filename])
